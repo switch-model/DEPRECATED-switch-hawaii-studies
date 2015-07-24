9d6ed9bbed0d5827074921766403346ef5b815d9
Matthias Fripp <mfripp@hawaii.edu>
2015-07-23 21:51:00 -1000

    moved code that is shared among scenarios to a separate switch-hawaii-core repository

--- models/oahu_2007_dr_updated/solve.py
+++ models/oahu_2007_dr_updated/solve.py
@@ -1,16 +1,32 @@
 #!/usr/local/bin/python
 
 import sys, os, time, traceback
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../switch')))
 
 from pyomo.environ import *
 from pyomo.opt import SolverFactory, SolverStatus, TerminationCondition
 
+def add_relative_path(*parts):
+    """ Adds a new path to sys.path.
+    The path should be specified relative to the current module, 
+    and specified as a list of directories to traverse to reach the
+    final destination."""
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), *parts)))
+    
+# note: switch and switch-hawaii should normally be cloned 
+# into the directory for each scenario, and then during active 
+# developmnet they should be refreshed periodically and the scenario 
+# files should also be updated to address any changes. This way, 
+# when a scenario is archived, it retains the appropriate versions 
+# of switch and switch-hawaii, so it can be re-run if needed.
+
+add_relative_path('switch')   # standard switch model
 import switch_mod.utilities as utilities
 from switch_mod.utilities import define_AbstractModel
 
+add_relative_path('switch-hawaii-core') # common components of switch-hawaii
 import util
+
+add_relative_path('.') # components for this particular study
 import demand_response
 
 opt = SolverFactory("cplex", solver_io="nl")
@@ -21,13 +37,36 @@ opt = SolverFactory("cplex", solver_io="nl")
 # number of units available
 opt.options['mipgap'] = 0.001
 
+print "loading model..."
+
+# tell pyomo to make all parameters mutable by default
+# (I only need to change lz_demand_mw, but this is currently the only way to make
+# any parameter mutable without changing the core code.)
+# NOTE: this causes errors like "TypeError: unhashable type: '_ParamData'" 
+# when any parameters are used as indexes into a set (e.g., m.ts_scale_to_period[ts])
+# This may be a pyomo bug, but it's hard to work around in the short term.
+# Param.DefaultMutable = True
+
+switch_model = define_AbstractModel(
+    'switch_mod', 'fuel_cost', 'project.no_commit', 
+    #'project.unitcommit', 'project.unitcommit.discrete', 
+    'demand_response', 'rps',
+    'switch_patch'
+)
+#switch_model.iis = Suffix(direction=Suffix.IMPORT)
+switch_model.dual = Suffix(direction=Suffix.IMPORT)
+
+switch_instance = switch_model.load_inputs(inputs_dir="inputs")
+
+results = None  # defined globally for convenient access in interactive session
+
 def iterate():
     global switch_model, switch_instance, results
     # NOTE: some evil magic in demand_response 
     # turns on bid adjusting behavior if "adj_bad_bids" is in the tag
     # or bid dropping behavior if "drop_bad_bids" is in the tag
-    tag = 'accel_rps_fixed_ce_25'
-    for i in range(25):
+    tag = 'accel_rps_fixed_ce_1'
+    for i in range(1):
         # solve the model repeatedly, iterating with a new demand function
         solve_once()
 
@@ -161,78 +200,6 @@ def solve_once():
             for lz in switch_instance.LOAD_ZONES
             for tp in switch_instance.TS_TPS[switch_instance.TIMESERIES[2]]]
         
-def patch_switch(switch_model):
-    """Make various changes to the model to facilitate reporting and avoid unwanted behavior"""
-    
-    # def get_proj_dispatch_val(component, p, t, default=0.0):
-    #     # return the value of a dispatch-related component for project p at timepoint t.
-    #     # use the default value if the timepoint is invalid for this project.
-    #     # note: this is slow, but not as slow as filtering the whole when doing sums, like this:
-    #     # [sum(
-    #     #     m.DispatchProj[p, t]
-    #     #     for p, t_ in m.PROJ_DISPATCH_POINTS
-    #     #     if t_ == t and p in m.FUEL_BASED_PROJECTS and m.proj_fuel[p] == f
-    #     # ) for f in m.FUELS for t in m.TIMEPOINTS]
-    #     return component[p, t] if (p, t) in component.index_set() else default
-    
-    # define parallel dispatch-related components with values for all timepoints. 
-    # (This requires more memory than using a function, but may be faster for repeated 
-    # evaluation; e.g., it only checks the index set once, at initialization time, and 
-    # it may allow loops to run in C instead of Python code.)
-    switch_model.DispatchProj_AllTimePoints = Expression(
-        switch_model.PROJECTS, switch_model.TIMEPOINTS, 
-        rule=lambda m, p, t:
-            m.DispatchProj[p, t] if (p, t) in m.PROJ_DISPATCH_POINTS
-                else 0.0
-    )
-    switch_model.DispatchUpperLimit_AllTimePoints = Expression(
-        switch_model.PROJECTS, switch_model.TIMEPOINTS, 
-        rule=lambda m, p, t:
-            m.DispatchUpperLimit[p, t] if (p, t) in m.PROJ_DISPATCH_POINTS
-                else 0.0
-    )
-
-    # create lists of projects by energy source
-    switch_model.PROJECTS_BY_FUEL = Set(switch_model.FUELS, initialize=lambda m, f:
-        # we sort this to help with display, but that may not actually have any effect
-        sorted([p for p in m.FUEL_BASED_PROJECTS if m.proj_fuel[p] == f])
-    )
-    switch_model.PROJECTS_BY_NON_FUEL_ENERGY_SOURCE = Set(switch_model.NON_FUEL_ENERGY_SOURCES, initialize=lambda m, s:
-        # we sort this to help with display, but that may not actually have any effect
-        sorted([p for p in m.NON_FUEL_BASED_PROJECTS if m.proj_non_fuel_energy_source[p] == s])
-    )
-
-    # constrain DumpPower to zero, so we can track curtailment better
-    switch_model.No_Dump_Power = Constraint(switch_model.LOAD_ZONES, switch_model.TIMEPOINTS,
-        rule=lambda m, z, t: m.DumpPower[z, t] == 0.0
-    )
-
-
-####################
-# Main execution code
-
-print "loading model..."
-
-# tell pyomo to make all parameters mutable by default
-# (I only need to change lz_demand_mw, but this is currently the only way to make
-# any parameter mutable without changing the core code.)
-# NOTE: this causes errors like "TypeError: unhashable type: '_ParamData'" 
-# when any parameters are used as indexes into a set (e.g., m.ts_scale_to_period[ts])
-# This may be a pyomo bug, but it's hard to work around in the short term.
-# Param.DefaultMutable = True
-
-switch_model = define_AbstractModel(
-    'switch_mod', 'fuel_cost', 'project.no_commit', #'project.unitcommit', 'project.unitcommit.discrete', 
-    'demand_response', 'rps'
-)
-#switch_model.iis = Suffix(direction=Suffix.IMPORT)
-switch_model.dual = Suffix(direction=Suffix.IMPORT)
-
-patch_switch(switch_model)
-
-switch_instance = switch_model.load_inputs(inputs_dir="inputs")
-
-results = None
 
 ###############
     
