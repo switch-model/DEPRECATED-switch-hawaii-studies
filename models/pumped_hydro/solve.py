3047ee1dfa9ccfc21c8bd8ba25051872da4a869b
Matthias Fripp <mfripp@hawaii.edu>
2015-07-23 23:17:57 -1000

    created simple solve.py for pumped_hydro

--- models/pumped_hydro/solve.py
+++ models/pumped_hydro/solve.py
@@ -27,7 +27,6 @@ add_relative_path('switch-hawaii-core') # common components of switch-hawaii
 import util
 
 add_relative_path('.') # components for this particular study
-import demand_response
 
 opt = SolverFactory("cplex", solver_io="nl")
 # tell cplex to find an irreducible infeasible set (and report it)
@@ -36,6 +35,9 @@ opt = SolverFactory("cplex", solver_io="nl")
 # relax the integrality constraints, to allow commitment constraints to match up with 
 # number of units available
 opt.options['mipgap'] = 0.001
+
+# label to attach to results files
+tag = None
 
 print "loading model..."
 
@@ -50,73 +52,74 @@ print "loading model..."
 switch_model = define_AbstractModel(
     'switch_mod', 'fuel_cost', 'project.no_commit', 
     #'project.unitcommit', 'project.unitcommit.discrete', 
-    'demand_response', 'rps',
+    #'demand_response', 'rps',
     'switch_patch'
 )
-#switch_model.iis = Suffix(direction=Suffix.IMPORT)
+switch_model.iis = Suffix(direction=Suffix.IMPORT)
 switch_model.dual = Suffix(direction=Suffix.IMPORT)
 
 switch_instance = switch_model.load_inputs(inputs_dir="inputs")
 
 results = None  # defined globally for convenient access in interactive session
 
-def iterate():
-    global switch_model, switch_instance, results
-    # NOTE: some evil magic in demand_response 
-    # turns on bid adjusting behavior if "adj_bad_bids" is in the tag
-    # or bid dropping behavior if "drop_bad_bids" is in the tag
-    tag = 'accel_rps_fixed_ce_1'
-    for i in range(1):
-        # solve the model repeatedly, iterating with a new demand function
-        solve_once()
+def solve():
+    global switch_model, switch_instance, results, tag
 
-        # if i <= 24 or i % 5 == 0:
-        #     # save time by only writing results every 5 iterations
-        #     write_results(tag=tag+'_'+str(i))
-        write_results(tag=tag+'_'+str(i))
+    print "solving model..."
+    start = time.time()
+    results = opt.solve(switch_instance, keepfiles=False, tee=True, 
+        symbolic_solver_labels=True, suffixes=['dual', 'iis'])
+    print "Total time in solver: {t}s".format(t=time.time()-start)
+
+    # results.write()
+    switch_instance.solutions.load_from(results)
+
+    if results.solver.termination_condition == TerminationCondition.infeasible:
+        print "Model was infeasible; Irreducible Infeasible Set (IIS) returned by solver:"
+        print "\n".join(c.cname() for c in switch_instance.iis)
+        if util.interactive_session:
+            print "Unsolved model is available as switch_instance."
+        raise RuntimeError("Infeasible model")
+
+
+    if util.interactive_session:
+        print "Model solved successfully."
+        print "Solved model is available as switch_instance."
+    
+    print "\n\n======================================================="
+    print "Solved model"
+    print "======================================================="
+    print "Total cost: ${v:,.0f}".format(v=value(switch_instance.Minimize_System_Cost))
         
-        #import pdb; pdb.set_trace()
-        
-        print "attaching new demand bid to model"
-        demand_response.update_demand(switch_instance, tag)
-        switch_instance.preprocess()
+    write_results(tag=tag)
+
 
 def write_results(tag=None):
+    # format the tag to append to file names (if any)
     if tag is not None:
         t = "_"+str(tag)
     else:
         t = ""
+        
+    # make sure there's a valid output directory
+    output_dir = "outputs"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)        
+    if not os.path.isdir(output_dir):
+        raise RuntimeError("Unable to create output directory {dir}.".format(dir=output_dir))
+    
     # write out results
     util.write_table(switch_instance, switch_instance.TIMEPOINTS,
-        output_file=os.path.join("outputs", "dispatch{t}.txt".format(t=t)), 
+        output_file=os.path.join(output_dir, "dispatch{t}.txt".format(t=t)), 
         headings=("timepoint_label",)+tuple(switch_instance.PROJECTS),
         values=lambda m, t: (m.tp_timestamp[t],) + tuple(
             m.DispatchProj_AllTimePoints[p, t] 
             for p in m.PROJECTS
         )
     )
-    util.write_table(switch_instance, switch_instance.TIMEPOINTS, 
-        output_file=os.path.join("outputs", "load_balance{t}.txt".format(t=t)), 
-        headings=
-            ("timepoint_label",)
-            +tuple(switch_instance.LZ_Energy_Components_Produce)
-            +tuple(switch_instance.LZ_Energy_Components_Consume)
-            +("marginal_cost",),
-        values=lambda m, t: 
-            (m.tp_timestamp[t],) 
-            +tuple(sum(getattr(m, component)[lz, t] for lz in m.LOAD_ZONES)
-                    for component in m.LZ_Energy_Components_Produce)
-            +tuple(sum(getattr(m, component)[lz, t] for lz in m.LOAD_ZONES)
-                    for component in m.LZ_Energy_Components_Consume)
-            +(sum(m.dual[m.Energy_Balance[lz, t]]/m.bring_timepoint_costs_to_base_year[t] for lz in m.LOAD_ZONES)
-                /len(m.LOAD_ZONES),)
-    )    
-    
-    # note: the queries below could be sped up slightly by defining an indexed set 
-    # of projects that use each particular energy source
     util.write_table(
         switch_instance, switch_instance.LOAD_ZONES, switch_instance.TIMEPOINTS, 
-        output_file=os.path.join("outputs", "energy_sources{t}.txt".format(t=t)), 
+        output_file=os.path.join(output_dir, "energy_sources{t}.txt".format(t=t)), 
         headings=
             ("load_zone", "timepoint_label")
             +tuple(switch_instance.FUELS)
@@ -148,65 +151,19 @@ def write_results(tag=None):
                     for component in m.LZ_Energy_Components_Consume)
             +(m.dual[m.Energy_Balance[z, t]]/m.bring_timepoint_costs_to_base_year[t],)
     )
-    util.write_table(switch_instance, switch_instance.LOAD_ZONES, switch_instance.TIMEPOINTS, 
-        output_file=os.path.join("outputs", "marginal_cost{t}.txt".format(t=t)), 
-        headings=("timepoint_label", "load_zone", "marginal_cost"),
-        values=lambda m, lz, tp: 
-            (m.tp_timestamp[tp], lz, m.dual[m.Energy_Balance[lz, tp]]/m.bring_timepoint_costs_to_base_year[tp])
-    )
+
     # import pprint
     # b=[(pr, pe, value(m.BuildProj[pr, pe]), m.proj_gen_tech[pr], m.proj_overnight_cost[pr, pe]) for (pr, pe) in m.BuildProj if value(m.BuildProj[pr, pe]) > 0]
     # bt=set(x[3] for x in b) # technologies
     # pprint([(t, sum(x[2] for x in b if x[3]==t), sum(x[4] for x in b if x[3]==t)/sum(1.0 for x in b if x[3]==t)) for t in bt])
 
-    
-def solve_once():
-    global switch_model, switch_instance, results
-
-    print "solving model..."
-    start = time.time()
-    results = opt.solve(switch_instance, keepfiles=False, tee=True, 
-        symbolic_solver_labels=True, suffixes=['dual'])
-    print "Total time in solver: {t}s".format(t=time.time()-start)
-
-    # results.write()
-    switch_instance.solutions.load_from(results)
-    # if not: raise RuntimeError("Unable to load solver results. Problem may be infeasible.")
-
-    if results.solver.termination_condition == TerminationCondition.infeasible:
-        print "Model was infeasible; Irreducible Infeasible Set (IIS) returned by solver:"
-        #print "\n".join(c.cname() for c in switch_instance.iis)
-        if util.interactive_session:
-            print "Unsolved model is available as switch_instance."
-        raise RuntimeError("Infeasible model")
-
-
-    if util.interactive_session:
-        print "Model solved successfully."
-        print "Solved model is available as switch_instance."
-    
-    print "\n\n======================================================="
-    print "Solved model"
-    print "======================================================="
-    print "Total cost: ${v:,.0f}".format(v=value(switch_instance.Minimize_System_Cost))
-    print "marginal costs (first day):"
-    print [switch_instance.dual[switch_instance.Energy_Balance[lz, tp]] \
-                /switch_instance.bring_timepoint_costs_to_base_year[tp] 
-            for lz in switch_instance.LOAD_ZONES
-            for tp in switch_instance.TS_TPS[switch_instance.TIMESERIES[1]]]
-    print "marginal costs (second day):"
-    print [switch_instance.dual[switch_instance.Energy_Balance[lz, tp]] \
-                /switch_instance.bring_timepoint_costs_to_base_year[tp] 
-            for lz in switch_instance.LOAD_ZONES
-            for tp in switch_instance.TS_TPS[switch_instance.TIMESERIES[2]]]
-        
 
 ###############
     
 if __name__ == '__main__':
     # catch errors so the user can continue with a solved model
     try:
-        iterate()
+        solve()
     except Exception, e:
         traceback.print_exc()
         print "ERROR:", e
