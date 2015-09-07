5cc68cb2ef37f607a7171c8f8644c4536605553b
Matthias Fripp <mfripp@gmail.com>
2015-09-07 06:53:24 -1000

    finished adding multi-fuel capability

---
+++ models/dr/rps.py
@@ -0,0 +1,60 @@
+import bisect
+from pprint import pprint
+from pyomo.environ import *
+import switch_mod.utilities as utilities
+
+
+def define_components(m):
+    """
+
+    """
+    ###################
+    # RPS calculation (very crude)
+    ##################
+    
+    rps_data = {2015: 0.15, 2020: 0.30, 2030: 0.40, 2040: 0.70, 2045: 1.00, 2100: 1.00} # must extend past last period
+    rps_energy_sources = ['WND', 'SUN', 'Biocrude', 'Biodiesel', 'MLG']
+
+    m.RPS_ENERGY_SOURCES = Set(initialize=rps_energy_sources)
+    m.RPS_DATES = Set(initialize=sorted(rps_data.keys()), ordered=True)
+    m.rps_targets = Param(m.RPS_DATES, initialize=lambda m, y: rps_data[y])
+
+    def rps_target_for_period_rule(m, p):
+        # find the last target that is in effect before the end of the period
+        # note that RPS_DATES is 1-based
+        # this could use code like the following, but it is hard to read and tends to fail
+        # m.rps_targets[m.RPS_DATES[bisect.bisect_right(m.RPS_DATES, period)-1]]
+        latest_target = max(y for y in m.RPS_DATES if y < p + m.period_length_years[p])
+        return m.rps_targets[latest_target]
+    m.rps_target_for_period = Param(m.PERIODS, initialize=rps_target_for_period_rule)
+
+    m.RPS_Enforce = Constraint(m.PERIODS, rule=lambda m, per:
+        ( # RE Sources
+            sum(
+                m.DispatchProjByFuel[proj, tp, f] 
+                for proj, tp, f in m.PROJ_FUEL_DISPATCH_POINTS
+                if m.tp_period[tp] == per and proj in m.FUEL_BASED_PROJECTS and f in m.RPS_ENERGY_SOURCES
+            )
+            +
+            sum(
+                m.DispatchProj[proj, tp] 
+                for proj, tp in m.PROJ_DISPATCH_POINTS
+                if m.tp_period[tp] == per and proj in m.NON_FUEL_BASED_PROJECTS 
+                    and m.g_energy_source[m.proj_gen_tech[proj]] in m.RPS_ENERGY_SOURCES
+            )
+            +
+            sum(    # assume DumpPower is curtailed renewable energy (note: DumpPower is negative)
+                m.DumpPower[lz, tp]
+                for lz in m.LOAD_ZONES for tp in m.TIMEPOINTS
+                if m.tp_period[tp] == per
+            )
+        )
+        >=
+        m.rps_target_for_period[per] 
+        * sum(  # all energy sources
+            m.DispatchProj[proj, tp] 
+            for (proj, tp) in m.PROJ_DISPATCH_POINTS
+            if m.tp_period[tp] == per
+        )
+    )
+
