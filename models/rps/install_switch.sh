b96d309795e3f63760906731cd9e21f81d5cad79
Matthias Fripp <mfripp@hawaii.edu>
2016-01-27 17:54:12 -1000

    corrected repository URLS in install_switch.sh

--- models/rps/install_switch.sh
+++ models/rps/install_switch.sh
@@ -5,12 +5,12 @@
 
 # currently, this installs the versions that were used for Matthias Fripp's 100% renewables white paper on 2016-01-15.
 
-git clone https://github.com/switch-model/switch/switch
+git clone https://github.com/switch-model/switch.git
 cd switch
 git checkout 7c7a7dc582d0949db89a3eac3e49fbfb29e9c673
 cd ..
 
-git clone https://github.com/switch-model/switch/switch-hawaii-core
+git clone https://github.com/switch-model/switch-hawaii-core.git
 cd switch-hawaii-core
 git checkout 14726eab6b11ae2ea1cef188d1e602a246805f25
 cd ..
