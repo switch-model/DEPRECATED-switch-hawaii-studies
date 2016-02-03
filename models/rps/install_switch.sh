1826f4ddf19a3c5b44527c66c1693c10bf7929ab
Matthias Fripp <mfripp@hawaii.edu>
2016-02-03 10:45:30 -1000

    install_switch.sh installs latest versions of dependencies

--- models/rps/install_switch.sh
+++ models/rps/install_switch.sh
@@ -3,17 +3,8 @@
 # this script will install the switch and switch-hawaii-core repositories
 # in subdirectories under this one
 
-# this restores the version used for Matthias Fripp's presentation to the EUCI conference
-# on 2016-01-28.
-
 git clone https://github.com/switch-model/switch.git
-cd switch
-git checkout 322f065345b4b7bb6d9d36914f3b47776a10fa1c
-cd ..
 
 git clone https://github.com/switch-model/switch-hawaii-core.git
-cd switch-hawaii-core
-git checkout b9b14ea20f148dca0347ac4761f841bf0e31f84e
-cd ..
 
 echo "switch and switch-hawaii-core repositories have been installed"
